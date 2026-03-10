00b4ab3609e0b25bbcf53139de29b37a8a1552d6
Samarth123 <samarthadkitte@gmail.com>
2026-03-10 23:22:38 +0530

    Added frontend files properly

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/message")
+      .then(res => res.text())
+      .then(data => setMessage(data));
+  }, []);
+
+  return (
+    <div style={{textAlign:"center", marginTop:"100px"}}>
+      <h1>React + Spring Boot</h1>
+      <h2>{message}</h2>
+    </div>
+  );
+
+}
+
+export default App;
